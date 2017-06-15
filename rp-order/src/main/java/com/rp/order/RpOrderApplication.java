af71e042f49117b3658b223ca21d755362deceef
hamcheol <hamcheol@naver.com>
2017-06-15 18:28:15 +0900

    ...

---
+++ rp-order/src/main/java/com/rp/order/RpOrderApplication.java
@@ -0,0 +1,12 @@
+package com.rp.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpOrderApplication.class, args);
+	}
+}
