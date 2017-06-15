c041d14c159417aca9b898cd8fdf70e2ab17e70c
hamcheol <hamcheol@naver.com>
2017-06-15 19:08:43 +0900

    ...

---
+++ rp-order/src/main/java/com/rp/order/config/BizEnum.java
@@ -0,0 +1,50 @@
+package com.rp.order.config;
+
+public class BizEnum {
+	public static enum ORDER_STAT {
+		결제완료("OS001"),
+		배송중("OS002"),
+		배송완료("OS003"),
+		구매확정("OS004");
+		
+		private String code;
+		private String name;
+		
+		private ORDER_STAT(String code) {
+			this.code = code;
+			this.name = name();
+		}
+		
+		public String getCode() {
+			return code;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+	
+	public static enum ORDER_EVENT {
+		결제완료("OE001"),
+		배송중("OE002"),
+		배송완료("OE003"),
+		구매확정("OE004");
+		
+		private String code;
+		private String name;
+		
+		private ORDER_EVENT(String code) {
+			this.code = code;
+			this.name = name();
+		}
+		
+		public String getCode() {
+			return code;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+
+}
